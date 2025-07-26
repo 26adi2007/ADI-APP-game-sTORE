2e6047f0c2d592a2fb050d4b1698a2876ea0bd90
26adi2007 <rajputrupesh306@gmail.com>
2025-07-26 16:06:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,14 @@ const apps = [
     link: "https://play.google.com/store/apps/details?id=com.kiloo.subwaysurf"
   },
   {
+    name: "BGMI: Online Multiplayer Game",
+    rating: "4.2",
+    size: "1.1GB",
+    category: "Battle Royale",
+    img: "BGMI.jpg",
+    link: "https://play.google.com/store/apps/details?id=com.pubg.imobile"
+  },
+  {
     name: "Ludo King",
     rating: "4.2",
     size: "60MB",
@@ -70,7 +78,113 @@ const apps = [
     category: "Sports",
     img: "8 Ball Pool.jpeg",
     link: "https://play.google.com/store/apps/details?id=com.miniclip.eightballpool"
-  }
+  },
+  {
+    name: "Carrom Pool",
+    rating: "4.3",
+    size: "45MB",
+    category: "Board",
+    img: "Carrom Pool.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.miniclip.carrom"
+  },
+  {
+    name: "Snakes and Ladders King",
+    rating: "4.1",
+    size: "35MB",
+    category: "Board",
+    img: "Snakes and Ladders King.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.gametion.snakesladders&pcampaignid=web_share"
+  },
+  {
+    name: "Chess - Play and Learn",
+    rating: "4.5",
+    size: "52MB",
+    category: "Board",
+    img: "Chess.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.chess"
+  },
+ 
+  {
+    name: "Among Us",
+    rating: "4.4",
+    size: "125MB",
+    category: "Casual",
+    img: "Among Us.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.innersloth.spacemafia"
+  },
+  {
+    name: "Real Cricket™ 20",
+    rating: "4.2",
+    size: "500MB",
+    category: "Sports",
+    img: "Real Cricket 20.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.nautilus.RealCricket3D"
+  },
+  {
+    name: "Garena Call of Duty®",
+    rating: "4.3",
+    size: "2.1GB",
+    category: "Shooter",
+    img: "Call of Duty.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.garena.game.codm"
+  },
+  {
+    name: "Hill Climb Racing",
+    rating: "4.4",
+    size: "80MB",
+    category: "Racing",
+    img: "Hill Climb Racing.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.fingersoft.hillclimb"
+  },
+  {
+    name: "Real Racing 3",
+    rating: "4.5",
+    size: "1.5GB",
+    category: "Racing",
+    img: "Real Racing 3.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.ea.games.r3_row"
+  },
+  {
+    name: "Tic Tac Toe Glow",
+    rating: "4.3",
+    size: "19MB",
+    category: "Puzzle",
+    img: "Tic Tac Toe Glow.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.arcsys.tictactoe"
+  },
+ 
+  {
+    name: "2048",
+    rating: "4.6",
+    size: "12MB",
+    category: "Puzzle",
+    img: "2048.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.androbaby.game2048"
+  },
+  {
+    name: "Fruit Ninja®",
+    rating: "4.5",
+    size: "150MB",
+    category: "Arcade",
+    img: "Fruit Ninja.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.halfbrick.fruitninja"
+  },
+  {
+    name: "Brain Out: Can you pass it?",
+    rating: "4.3",
+    size: "97MB",
+    category: "Puzzle",
+    img: "Brain Out.jpeg",
+    link: "https://play.google.com/store/apps/details?id=com.mind.quiz.brain.out"
+  },
+  {
+    name: "Pou",
+    rating: "4.4",
+    size: "23MB",
+    category: "Casual",
+    img: "Pou.jpeg",
+    link: "https://play.google.com/store/apps/details?id=me.pou.app"
+  },
 
 ];
 
@@ -99,4 +213,3 @@ document.getElementById('search').addEventListener('input', function() {
 
 // Initial render
 renderApps(apps);
-
